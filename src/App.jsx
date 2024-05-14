9d7089648f861d75accc27c186ae8ffdd16db6cd
guilherme zanollo <guilherme.zanollo@gmail.com>
2024-05-14 17:18:44 -0300

    Atualização do projeto com bundler da DB1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
+import { useState } from 'react'
 import './App.css'
 
 import Clock from './components/Clock'
 import Step from './components/Step'
 
 function App() {
-
+  const [showStep, setShowStep] = useState(true);
+  const handleShowHideStep = () => {
+    setShowStep(!showStep);
+  }
+ 
   return (
     <div>
         <Clock startDate={new Date(2001, 9, 15, 20, 30, 0)} />
 
         <br />
 
-        <Step />
+        {showStep ? <Step /> : null}
+
+        <button onClick={handleShowHideStep}>Exibir/Ocultar step3</button>
     </div>
   )
 }
--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -1,7 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const useDidMount = (callback) => useEffect(callback, []);
 
 const Step = () => {
     const [step, setStep] = useState(0);
+
+    useDidMount(() => {
+        console.log('Mount: ', step);
+        return () => {
+            console.log('Unmount: ', step);
+        }
+    });       // VAI EXECUTAR APENAS UMAS VEZ
+
+    useEffect(() => {
+        console.log('Setup: ', step);
+        if (step ===10) {
+            alert('Chegou no 10');
+        }
+        return () => {
+            console.log('Cleaning: ', step);
+        }
+    }, [step]); // reagindo
+
     const handleDecrement = () => {
         setStep(step - 1);
     }
